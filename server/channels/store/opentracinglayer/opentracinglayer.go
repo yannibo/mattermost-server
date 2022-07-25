0bb4fe4ef0ff2be4503b6a2e2239896f543d0ffa
Vishal Choudhary <vish9812@gmail.com>
2022-07-25 18:45:50 +0530

    Include IsFollowing in pinnedPosts

--- server/channels/store/opentracinglayer/opentracinglayer.go
+++ server/channels/store/opentracinglayer/opentracinglayer.go
@@ -1668,7 +1668,7 @@ func (s *OpenTracingLayerChannelStore) GetPinnedPostCount(channelID string, allo
 	return result, err
 }
 
-func (s *OpenTracingLayerChannelStore) GetPinnedPosts(channelID string) (*model.PostList, error) {
+func (s *OpenTracingLayerChannelStore) GetPinnedPosts(channelID string, userID string) (*model.PostList, error) {
 	origCtx := s.Root.Store.Context()
 	span, newCtx := tracing.StartSpanWithParentByContext(s.Root.Store.Context(), "ChannelStore.GetPinnedPosts")
 	s.Root.Store.SetContext(newCtx)
@@ -1677,7 +1677,7 @@ func (s *OpenTracingLayerChannelStore) GetPinnedPosts(channelID string) (*model.
 	}()
 
 	defer span.Finish()
-	result, err := s.ChannelStore.GetPinnedPosts(channelID)
+	result, err := s.ChannelStore.GetPinnedPosts(channelID, userID)
 	if err != nil {
 		span.LogFields(spanlog.Error(err))
 		ext.Error.Set(span, true)
